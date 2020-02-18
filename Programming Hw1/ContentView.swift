9a2ddd6ac520a18543177fc0207f6ce6d5fd952e
Jakob Sabatula <j.sabatula@setonhill.edu>
2020-02-18 12:17:23 -0500

    ready

---
+++ Programming Hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Programming Hw1
+//
+//  Created by Jakob Sabatula on 2/3/20.
+//  Copyright © 2020 Jakob Sabatula. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Programming Hw1Tests/Programming_Hw1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Programming_Hw1Tests.swift
+//  Programming Hw1Tests
+//
+//  Created by Jakob Sabatula on 2/3/20.
+//  Copyright © 2020 Jakob Sabatula. All rights reserved.
+//
+
+import XCTest
+@testable import Programming_Hw1
+
+class Programming_Hw1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
